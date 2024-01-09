61fd9a10d5ca8f6766a1abbbaf9eb09f2369b343
Alex <ryzhko_alex@mail.ru>
2024-01-09 17:49:29 +0300

    usage stringBulder instead of array of strings

--- src/main/java/org/example/MinimalDistance.java
+++ src/main/java/org/example/MinimalDistance.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class MinimalDistance {
     public static void main(String[] args) {
         try {
@@ -10,19 +12,9 @@ public class MinimalDistance {
         }
     }
 
-    public static String[] insertIntoArray(final String[] originalArray, final int insertionIndex, final String newItem) {
-        int originalLength = originalArray.length;
-        String[] newArray = new String[originalLength + 1];
-        System.arraycopy(originalArray, 0, newArray, 0, insertionIndex);
-        newArray[insertionIndex] = newItem;
-        System.arraycopy(originalArray, insertionIndex, newArray, insertionIndex + 1, originalLength - insertionIndex);
-
-        return newArray;
-    }
-
     public static void minimalDistance(final String source, final String target) {
-        int sourceLength = source.length();
-        int targetLength = target.length();
+        final int sourceLength = source.length();
+        final int targetLength = target.length();
         int[][] editDistanceMatrix = new int[sourceLength + 1][targetLength + 1];
 
         for (int i = 0; i <= sourceLength; i++) {
@@ -34,9 +26,9 @@ public class MinimalDistance {
 
         for (int i = 1; i <= sourceLength; i++) {
             for (int j = 1; j <= targetLength; j++) {
-                int deletion = editDistanceMatrix[i][j - 1] + 1;
-                int insertion = editDistanceMatrix[i - 1][j] + 1;
-                int substitution = editDistanceMatrix[i - 1][j - 1] + (source.charAt(i - 1) == target.charAt(j - 1) ? 0 : 1);
+                final int deletion = editDistanceMatrix[i][j - 1] + 1;
+                final int insertion = editDistanceMatrix[i - 1][j] + 1;
+                final int substitution = editDistanceMatrix[i - 1][j - 1] + (source.charAt(i - 1) == target.charAt(j - 1) ? 0 : 1);
                 editDistanceMatrix[i][j] = Math.min(Math.min(deletion, insertion), substitution);
             }
         }
@@ -46,30 +38,30 @@ public class MinimalDistance {
 
         int currentSourceIndex = sourceLength;
         int currentTargetIndex = targetLength;
-        String[] transformedWord = target.split("");
+        StringBuilder transformedWord = new StringBuilder(target);
 
-        System.out.println(String.join("", transformedWord));
+        System.out.println(transformedWord.toString());
         while (distance > 0) {
             int deletion = editDistanceMatrix[currentSourceIndex][currentTargetIndex - 1];
             int insertion = editDistanceMatrix[currentSourceIndex - 1][currentTargetIndex];
             int substitution = editDistanceMatrix[currentSourceIndex - 1][currentTargetIndex - 1];
 
             if (substitution < distance) {
-                transformedWord[currentTargetIndex - 1] = Character.toString(source.charAt(currentSourceIndex - 1));
+                transformedWord.setCharAt(currentTargetIndex - 1, source.charAt(currentSourceIndex - 1));
                 currentSourceIndex--;
                 currentTargetIndex--;
                 distance = substitution;
-                System.out.println(String.join("", transformedWord));
+                System.out.println(transformedWord.toString());
             } else if (deletion < distance) {
-                transformedWord[currentTargetIndex - 1] = "";
+                transformedWord.deleteCharAt(currentTargetIndex - 1);
                 currentTargetIndex--;
                 distance = deletion;
-                System.out.println(String.join("", transformedWord));
+                System.out.println(transformedWord.toString());
             } else if (insertion < distance) {
-                transformedWord = insertIntoArray(transformedWord, currentTargetIndex, Character.toString(source.charAt(currentSourceIndex - 1)));
+                transformedWord.setCharAt(currentTargetIndex - 1, source.charAt(currentSourceIndex - 1));
                 currentSourceIndex--;
                 distance = insertion;
-                System.out.println(String.join("", transformedWord));
+                System.out.println(transformedWord.toString());
             } else {
                 currentSourceIndex--;
                 currentTargetIndex--;
